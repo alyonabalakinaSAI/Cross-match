78d08ef94dd52488711a6b1ebdfae50e05cce9b1
Alyona Balakina <37987097+alyonabalakinaSAI@users.noreply.github.com>
2019-12-23 12:34:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from setuptools import find_packages, setup
-import crossmatch
+import CrossMatch
 
 description = ""
 "Program for cross-match beteewn two catalogs: Pantheon sample of supernovae Ia and Meta-Catalog of X-ray Detected Cluster of galaxies. "
@@ -9,22 +9,21 @@ description = ""
 
 setup(
     name='crossmatch',
-    version=crossmatch.__version__,
+    version=CrossMatch.__version__,
     url='https://github.com/alyonabalakinaSAI/Cross-match',
     author='Elena Balakina',
     author_email='balakina.ea15@physics.msu.ru',
     description=description,
     packages=find_packages(),
-    data_files=[
-        ('FITRES', ['Pantheon.FITRES']),
-    ],
     test_suite='test',
     entry_points={
-        'console_scripts': ['crossmatch = crossmatch.crossmatch:main']
+        'console_scripts': ['crossmatch = crossmatch.CrossMatch:main']
     },
+    include_package_data=True,
     install_requires=['numpy', 'astropy', 'astroquery'],
     classifiers=[
         'Topic :: Astrophysics, Stellar evolution',
         'Programming Language :: Python :: 3.7',
     ],
 )
+

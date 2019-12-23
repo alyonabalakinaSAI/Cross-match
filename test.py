0cbc586e1207f8413cd3cfd103b893984ed293eb
Alyona Balakina <37987097+alyonabalakinaSAI@users.noreply.github.com>
2019-12-23 12:35:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import unittest
 from astropy.io import ascii
 from astropy.utils.diff import report_diff_values
 
-import Crossmatch.crossmatch as crossmatch
+from CrossMatch import crossmatch
 
 
 class CrossmatchTestCase(unittest.TestCase):
@@ -14,13 +14,14 @@ class CrossmatchTestCase(unittest.TestCase):
         crossmatch.main('-d ' + testfile + '/crossmatch/testcatalog')
         cat1 = ascii.read('cat1.csv', delimiter=',', guess=False, fast_reader=False)
         cat2 = ascii.read('cat2.csv', delimiter=',', guess=False, fast_reader=False)
-        table = crossmatch(cat1, cat2, 'RA1', 'DEC1', 'Z1', 'RA2', 'DEC2', 'Z2', 1000, use_z=True)
-        fin_table = table['NAME1', 'NAME2', 'RA1', 'RA2', 'DEC1', 'DEC2', 'DELTA_Z']
-        fin_table.write('TEST', format='ascii')
+        table = crossmatch(cat1, cat2, 'RA1', 'DEC1', 'Z1', 'RA2', 'DEC2', 'Z2', 20, use_z=True)
+        final_table = table['NAME1', 'NAME2', 'RA1', 'RA2', 'DEC1', 'DEC2', 'DELTA_Z']
+        final_table.write('TEST', format='ascii')
         cat3 = ascii.read('cat3.csv')
 
-        self.assertEqual(True, report_diff_values(cat3, fin_table))
+        self.assertEqual(True, report_diff_values(cat3, final_table))
 
 
 if __name__ == '__main__':
     unittest.main()
+
